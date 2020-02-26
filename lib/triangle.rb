2c71e81a0706fb25a61e9258c06f5faf5de0bca7
Karen Daniela <kdaniimora@gmail.com>
2020-02-26 20:47:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,43 @@
 class Triangle
-  attr_accessor :side1, :side2, :side3
-  
-  def initialiize(side1, side2, side3)
-    @side = side1
-    @side2 = side2
-    @side3 = side3
-end 
 
-def kind 
-end 
-  # write code here
+  attr_accessor :one, :two, :three
+  
+    def initialize (one, two, three)
+        @one = one
+        @two = two
+        @three = three
+    end
+    
+    def kind
+        illegal_triangle
+        
+        if one == two && one == three
+            :equilateral
+        elsif one == two || one == three || two == three
+            :isosceles
+        else
+            :scalene
+        end
+    end
+    
+    def illegal_triangle
+        if one <= 0 || two <= 0 || three <= 0
+            begin
+                raise TriangleError
+            end
+        elsif one == nil || two == nil || three == nil
+            begin
+                raise TriangleError
+            end
+        elsif one + two <= three || one + three <= two || three + two <= one
+            begin
+                raise TriangleError
+            end
+        end
+    end
+end
+
+class TriangleError < StandardError
+    
+end
 end
