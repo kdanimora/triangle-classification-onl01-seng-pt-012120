eb9d142490a7ca882ee736b5db11412bff0c3264
Karen Daniela <kdaniimora@gmail.com>
2020-02-26 20:47:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
+  attr_accessor :side1, :side2, :side3
+  
+  def initialiize(side1, side2, side3)
+    @side = side1
+    @side2 = side2
+    @side3 = side3
+end 
+
+def kind 
+end 
   # write code here
 end
